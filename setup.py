6eca345afd9d9916b360c003fe006f289999e8bd
Tzu-ping Chung <uranusjr@gmail.com>
2019-12-25 13:54:33 +0800

    Switch to Setuptools

---
+++ setup.py
@@ -0,0 +1,8 @@
+# Stub so we can run `python setup.py` commands during development. In time we
+# want to make setl be able to self-build and eliminating this file altogether.
+
+# NOTE: DO NOT ADD ANYTHING HERE; use setup.cfg instead.
+
+from setuptools import setup
+
+setup()
--- src/setl/__init__.py
+++ src/setl/__init__.py
@@ -1,4 +1,1 @@
-"""Packaging tool for PEP 518 projects with Setuptools as the backend.
-"""
-
 __version__ = "0.1.0"
