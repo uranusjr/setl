8738fc016c78910b1eb76f9b618003a50360bfbe
Tzu-ping Chung <uranusjr@gmail.com>
2019-12-27 17:13:16 +0800

    Add a warning for missing pyproject.toml
    I don't want to hack around the Setuptools problem (bug?) so let's
    prompt everyone for potential problems for now. This workaround won't
    hurt if Setuptools fixes it anyway.

--- src/setl/projects/hook.py
+++ src/setl/projects/hook.py
@@ -1,12 +1,20 @@
 __all__ = ["ProjectPEP517HookCallerMixin"]
 
 import functools
+import logging
 import pathlib
+import re
+import tarfile
 
 import pep517.wrappers
 
 from .build import BuildEnv, ProjectBuildManagementMixin
 from .meta import ProjectMetadataMixin
+
+
+logger = logging.getLogger(__name__)
+
+PYPROJECT_TOML_PATTERN = re.compile(r"^[^/]+/pyproject\.toml$")
 
 
 class ProjectPEP517HookCallerMixin(
@@ -18,10 +26,22 @@ class ProjectPEP517HookCallerMixin(
             self.root, self.build_backend, self.backend_path
         )
 
+    def _warn_if_pyproject_toml_not_present(self, target: str):
+        with tarfile.open(self.root.joinpath("dist", target)) as tf:
+            if any(PYPROJECT_TOML_PATTERN.match(n) for n in tf.getnames()):
+                return
+        logger.warn(
+            "pyproject.toml not found in `%s`. Add the following to "
+            "your MANIFEST.in:\n\n    include pyproject.toml\n\n"
+            "See: https://github.com/pypa/setuptools/issues/1632\n",
+            target,
+        )
+
     def build_sdist(self, env: BuildEnv) -> pathlib.Path:
         requirements = self.hooks.get_requires_for_build_sdist()
         self.install_build_requirements(env, requirements)
         target = self.hooks.build_sdist(self.root.joinpath("dist"))
+        self._warn_if_pyproject_toml_not_present(target)
         return self.root.joinpath("dist", target)
 
     def build_wheel(self, env: BuildEnv) -> pathlib.Path:
