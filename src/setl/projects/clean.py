32c66f5054ff7d6e6002e030e01e9c5b0119eb0d
Tzu-ping Chung <uranusjr@gmail.com>
2019-12-27 21:19:01 +0800

    Rewrite clean
    No longer try to uninstall the dev installation. Just leave it broken,
    like how Setuptools does it anyway.
    
    Fix dist discovery to find against the runtime env instead of the
    installed env (of Setl).

--- src/setl/projects/clean.py
+++ src/setl/projects/clean.py
@@ -6,83 +6,59 @@ import subprocess
 
 from typing import Iterator, Optional
 
-import pkg_resources
-
 from .build import BuildEnv
 from .dev import ProjectDevelopMixin
 
 
-def _get_distribution(
-    f: Iterator[str],
-) -> Optional[pkg_resources.Distribution]:
+def _get_name(f: Iterator[str]) -> Optional[str]:
     for line in f:
         if ":" not in line:  # End of metadata.
             break
         k, v = line.strip().split(":", 1)
-        if k.lower() != "name":
-            continue
-        try:
-            return pkg_resources.get_distribution(v.strip())
-        except pkg_resources.DistributionNotFound:
-            continue
+        if k.lower() == "name":
+            return v.strip()
     return None
 
 
-_CHECK_IN_PATH_CODE = """
+_GET_EGG_INFO_LOCATION_CODE = """
 import os
-import sys
+import pkg_resources
 
-location = os.environ["SETL_IS_INSTALLED_FOR_DEV_LOCATION"]
-for path in sys.path:
-    if os.path.commonprefix([path, location]) == path:
-        print("regular")
-        break
+name = os.environ["SELT_GET_EGG_INFO_LOCATION_NAME"]
+try:
+    dist = pkg_resources.get_distribution(name)
+except pkg_resources.DistributionNotFound:
+    pass
 else:
-    print("develop")
+    print(getattr(dist, "egg_info", ""))
 """
 
 
 class ProjectCleanMixin(ProjectDevelopMixin):
-    def _is_installed_for_dev(self, env: BuildEnv, path: str) -> bool:
+    def _clean_egg_info(self, env: BuildEnv):
+        name = _get_name(self.iter_metadata_for_development(env))
+        if not name:
+            return
+
+        environ = os.environ.copy()
+        environ["SELT_GET_EGG_INFO_LOCATION_NAME"] = name
         args = [
             os.fspath(env.interpreter),
             "-c",
-            _CHECK_IN_PATH_CODE,
+            _GET_EGG_INFO_LOCATION_CODE,
         ]
-        environ = os.environ.copy()
-        environ["SETL_IS_INSTALLED_FOR_DEV_LOCATION"] = path
-        output = subprocess.check_output(args, env=environ, text=True).strip()
+        info = subprocess.check_output(args, env=environ, text=True).strip()
 
-        return output == "develop"
-
-    def _clean_egg_info(self, env: BuildEnv, dist: pkg_resources.Distribution):
-        egg_info = getattr(dist, "egg_info", None)
-        if not egg_info or not os.path.exists(egg_info):
+        if not info or not os.path.isdir(info):
             return
-
-        # Uninstall the develop build (otherwise removing .egg-info would
-        # result in a broken environment).
-        if self._is_installed_for_dev(env, os.fspath(dist.location)):
-            args = [
-                os.fspath(env.interpreter),
-                "-m",
-                "pip",
-                "uninstall",
-                "--yes",
-                dist.project_name,
-            ]
-            subprocess.check_call(args)
-
-        shutil.rmtree(egg_info)
+        shutil.rmtree(info)
 
     def clean(self, env: BuildEnv):
         # Clean up the built .egg-info. This is probably a good idea.
         # https://github.com/pypa/setuptools/issues/1347
-        dist = _get_distribution(self.iter_metadata_for_development(env))
-        if dist:
-            self._clean_egg_info(env, dist)
+        self._clean_egg_info(env)
 
-        self.setuppy(env, "--quiet", "clean", "--all")
+        self.setuppy(env, "clean", "--all")
 
         # We don't just clean up here because the build environment is still
         # active, and the caller might want to use it after the call. Set a
