03281a47721e00dbdce9f247e8416ffef2b7689f
Tzu-ping Chung <uranusjr@gmail.com>
2020-02-18 03:59:06 +0800

    Remove pkg_resources dependency
    This is done by using pip list to locate the egg-info, and implement a
    manual lookup to find the egg-info directory. This is still
    Setuptools-specific, but at least we'll have a better migration path to
    switch to importlib-metadata afterwards.

--- src/setl/projects/clean.py
+++ src/setl/projects/clean.py
@@ -1,10 +1,13 @@
 __all__ = ["ProjectCleanMixin"]
 
+import json
 import os
 import shutil
 import subprocess
 
-from typing import Iterator, Optional
+from typing import Dict, Iterator, List, Optional
+
+from packaging.utils import canonicalize_name
 
 from .build import BuildEnv
 from .dev import ProjectDevelopMixin
@@ -20,22 +23,15 @@ def _get_name(f: Iterator[str]) -> Optional[str]:
     return None
 
 
-# TODO: Reliying on Setuptools being present in the isolated build
-# environment. This is fine since Setl is strictly a Setuptools frontend, but
-# for a generic PEP 517 frontend we need another way to find the .egg-info
-# directory. The .egg-link thing is very tricky though...
-_GET_EGG_INFO_LOCATION_CODE = """
-import os
-import sys
-import pkg_resources
-
-try:
-    dist = pkg_resources.get_distribution(sys.argv[1])
-except pkg_resources.DistributionNotFound:
-    pass
-else:
-    print(getattr(dist, "egg_info", ""))
-"""
+def _iter_egg_infos(entries: List[Dict[str, str]], name: str) -> Iterator[str]:
+    for entry in entries:
+        if name != canonicalize_name(entry["name"]):
+            continue
+        location = entry["location"]
+        for filename in os.listdir(location):
+            stem, ext = os.path.splitext(filename)
+            if canonicalize_name(stem) == name and ext == ".egg-info":
+                yield os.path.join(location, filename)
 
 
 class ProjectCleanMixin(ProjectDevelopMixin):
@@ -46,15 +42,17 @@ class ProjectCleanMixin(ProjectDevelopMixin):
 
         args = [
             os.fspath(env.interpreter),
-            "-c",
-            _GET_EGG_INFO_LOCATION_CODE,
-            name,
+            "-m",
+            "pip",
+            "list",
+            "--format=json",
+            "--editable",
+            "--verbose",  # Needed for the "location" field. (pypa/pip#7664)
         ]
-        info = subprocess.check_output(args, text=True).strip()
-
-        if not info or not os.path.isdir(info):
-            return
-        shutil.rmtree(info)
+        output = subprocess.check_output(args, text=True)
+        entries = json.loads(output)
+        for path in _iter_egg_infos(entries, canonicalize_name(name)):
+            shutil.rmtree(path)
 
     def clean(self, env: BuildEnv):
         # Clean up the built .egg-info. This is probably a good idea.
