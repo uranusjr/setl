4ed13f28a6bd1e27e4b8d4a3990d11a5653b7aee
Tzu-ping Chung <uranusjr@gmail.com>
2020-03-12 01:15:17 +0800

    Fix build-system defaults to match PEP 517

--- src/setl/projects/meta.py
+++ src/setl/projects/meta.py
@@ -9,30 +9,34 @@ from .base import BaseProject
 
 
 _DEFAULT_BUILD_REQUIREMENTS = ["setuptools", "wheel"]
+_DEFAULT_BUILD_BACKEND = "setuptools.build_meta:__legacy__"
 _DEFAULT_BUILD_SYSTEM = {
     "requires": _DEFAULT_BUILD_REQUIREMENTS,
-    "build-backend": "setuptools.build_meta:__legacy__",
+    "build-backend": _DEFAULT_BUILD_BACKEND,
 }
 
 
 class ProjectMetadataMixin(BaseProject):
     @cached_property.cached_property
-    def build_system(self) -> Dict[str, Any]:
+    def _build_system(self) -> Dict[str, Any]:
         with self.root.joinpath("pyproject.toml").open() as f:
             data = toml.load(f)
         try:
-            return data["build-system"]
+            build_system = data["build-system"]
         except KeyError:
             return _DEFAULT_BUILD_SYSTEM
+        if "build-backend" not in build_system:
+            return _DEFAULT_BUILD_SYSTEM
+        return build_system
 
     @property
     def build_requirements(self) -> List[str]:
-        return self.build_system.get("requires", _DEFAULT_BUILD_REQUIREMENTS)
+        return self._build_system.get("requires", _DEFAULT_BUILD_REQUIREMENTS)
 
     @property
     def build_backend(self) -> str:
-        return self.build_system.get("build-backend", "setuptools.build_meta")
+        return self._build_system.get("build-backend", _DEFAULT_BUILD_BACKEND)
 
     @property
     def backend_path(self) -> Optional[str]:
-        return self.build_system.get("backend-path")
+        return self._build_system.get("backend-path")
